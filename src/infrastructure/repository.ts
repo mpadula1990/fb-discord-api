7464a402f86948b16ac3849d967d1a517e801b25
mpadula1990 <44302654+mpadula1990@users.noreply.github.com>
2020-08-19 12:15:00 -0300

    repo class

--- src/infrastructure/repository.ts
+++ src/infrastructure/repository.ts
@@ -1,58 +1,77 @@
 const { Client } = require('pg');
 
-const connectionData = {
-    connectionString: 'postgres://eqgkqehyildlkr:12c493cc5e5ac1ace711b97c092cd45063a637c3b5d00c913d99fedb4077daf7@ec2-34-251-118-151.eu-west-1.compute.amazonaws.com:5432/ddmc9vcorm2ocn'
-};
+class DBRepository{
+  private connectionData = {
+      connectionString: 'postgres://eqgkqehyildlkr:12c493cc5e5ac1ace711b97c092cd45063a637c3b5d00c913d99fedb4077daf7@ec2-34-251-118-151.eu-west-1.compute.amazonaws.com:5432/ddmc9vcorm2ocn',
+      ssl: true
+  };
 
-const dbClient = new Client(connectionData);
+  private dbClient: any;
+  constructor(){
+    this.dbClient = new Client(this.connectionData);
+  }
 
-const getOperations = async (from:any, to:any) => {
-    try{
-      dbClient.connect();
-        var query: any = {
-            text: 'select o.id, o.operation_type, o.detail, o.operation_date, o.concept, o.memberid, r.id as rid, r.category, r.mount from operations o inner join registers r on o.id = r.operationid where o.operation_date >= $1 and o.operation_date <= $2',
-            values: [from, to]
-        };
-        var query = await dbClient.query(query);
-        return query.rows;
-    }catch(e) { console.log("asdasdasdAAAAAAA"); Promise.reject(e); }
+  public connect(){
+    this.dbClient.connect();
+  }
+
+  public disconnect(){
+    this.dbClient.end();
+  }
+
+  public getOperations = async (from:any, to:any): Promise<any> => {
+        await this.connect();
+          var query: any = {
+              text: 'select o.id, o.operation_type, o.detail, o.operation_date, o.concept, o.memberid, r.id as rid, r.category, r.mount from operations o inner join registers r on o.id = r.operationid where o.operation_date >= $1 and o.operation_date <= $2',
+              values: [from, to]
+          };
+
+          return new Promise((resolve, reject) => {
+            this.dbClient.query(query).then((result: any) => {
+              this.dbClient.end();
+              resolve(result);
+            }).catch((error: any) => {
+              this.dbClient.end();
+              reject(error);
+            });
+          });
+  }
+
+  public getTotal = async (category: any): Promise<number> => {
+      var total = 0;
+      try{
+          var query: any = {
+              text: 'SELECT * FROM "organization"."operations"',
+              values: [category]
+          };
+          var query = await this.dbClient.query(query);
+          return total;
+      }catch(e) { return Promise.reject(e); }
+  }
+
+  public registerOperation = () => {
+
+  }
+
+  public registerMember = () => {
+
+  }
+
+  public registerOrganization = () => {
+
+  }
+
+  public registerDeposit = () => {
+
+  }
+
+  public setGoal = () => {
+
+  }
+
+  public cancelOperation = () => {
+
+  }
 }
 
-const getTotal = async (category: any): Promise<number> => {
-    dbClient.connect();
-    var total = 0;
-    try{
-        var query: any = {
-            text: 'SELECT * FROM "organization"."operations"',
-            values: [category]
-        };
-        var query = await dbClient.query(query);
-        return total;
-    }catch(e) { return Promise.reject(e); }
-}
-
-const registerOperation = () => {
-
-}
-
-const registerMember = () => {
-
-}
-
-const registerOrganization = () => {
-
-}
-
-const registerDeposit = () => {
-
-}
-
-const setGoal = () => {
-
-}
-
-const cancelOperation = () => {
-
-}
-
-export {getOperations}
+export {DBRepository};
--- src/routes/api_routes.ts
+++ src/routes/api_routes.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
 import {v4 as uuid} from 'uuid';
 import cors from 'cors';
-import { getOperations } from '../infrastructure/repository';
+import { DBRepository } from '../infrastructure/repository';
 import moment from 'moment';
 
 class ApiRouter {
+  private repository: DBRepository;
     constructor(server: express.Express){
         const router = express.Router();
 
@@ -12,6 +13,7 @@ class ApiRouter {
 
         router.options('*', cors());
         server.use('/', router);
+        this.repository = new DBRepository();
     }
 
     private loadRoutes = (router: express.Router) => {
@@ -19,11 +21,8 @@ class ApiRouter {
             if(!req.params.from || !req.params.to){
               res.status(400).send('Bad Request!')
             }
-            console.log(req.params);
-            var from = moment(req.params.from, 'YYYYMMDD').format();
-            var to = moment(req.params.to, 'YYYYMMDD').format();
-            console.log(from, to);
-            getOperations(from, to).then(r=> {
+            this.repository.getOperations(moment(req.params.from, 'YYYYMMDD').format(), moment(req.params.to, 'YYYYMMDD').format()).then(r=> {
+              console.log(r, "r")
                 res.json(r);
             }).catch(e=> {
               res.status(500).send('Something broke! -> ' + e);
