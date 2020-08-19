6a2bfa3d55abcf06a4f63c23650c7e7e18fc224c
mpadula1990 <44302654+mpadula1990@users.noreply.github.com>
2020-08-19 11:02:30 -0300

    test

--- src/infrastructure/repository.ts
+++ src/infrastructure/repository.ts
@@ -1,25 +1,21 @@
 const { Client } = require('pg');
 
 const connectionData = {
-    user: 'eqgkqehyildlkr',
-    host: 'ec2-34-251-118-151.eu-west-1.compute.amazonaws.com',
-    database: 'ddmc9vcorm2ocn',
-    password: '12c493cc5e5ac1ace711b97c092cd45063a637c3b5d00c913d99fedb4077daf7',
-    port: 5432,
-}
+    connectionString: 'postgres://eqgkqehyildlkr:12c493cc5e5ac1ace711b97c092cd45063a637c3b5d00c913d99fedb4077daf7@ec2-34-251-118-151.eu-west-1.compute.amazonaws.com:5432/ddmc9vcorm2ocn'
+};
 
 const dbClient = new Client(connectionData);
 
-const getOperations = async (from:any, to:any, type: any) => {
-    dbClient.connect();
+const getOperations = async (from:any, to:any) => {
     try{
+      dbClient.connect();
         var query: any = {
-            text: 'SELECT * FROM "organization"."operations"',
-            values: [from, to, type]
+            text: 'select o.id, o.operation_type, o.detail, o.operation_date, o.concept, o.memberid, r.id as rid, r.category, r.mount from operations o inner join registers r on o.id = r.operationid where o.operation_date >= $1 and o.operation_date <= $2',
+            values: [from, to]
         };
         var query = await dbClient.query(query);
         return query.rows;
-    }catch(e) { return Promise.reject(e); }
+    }catch(e) { console.log("asdasdasdAAAAAAA"); Promise.reject(e); }
 }
 
 const getTotal = async (category: any): Promise<number> => {
@@ -56,7 +52,7 @@ const setGoal = () => {
 }
 
 const cancelOperation = () => {
-    
+
 }
 
 export {getOperations}
--- src/routes/api_routes.ts
+++ src/routes/api_routes.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import {v4 as uuid} from 'uuid';
 import cors from 'cors';
 import { getOperations } from '../infrastructure/repository';
+import moment from 'moment';
 
 class ApiRouter {
     constructor(server: express.Express){
@@ -13,11 +14,20 @@ class ApiRouter {
         server.use('/', router);
     }
 
-    private loadRoutes = (router: express.Router) =>{
-        router.get('/api', (req, res)=> {
-            getOperations().then(r=> {
+    private loadRoutes = (router: express.Router) => {
+        router.get('/api/operations/:from/:to', (req, res) => {
+            if(!req.params.from || !req.params.to){
+              res.status(400).send('Bad Request!')
+            }
+            console.log(req.params);
+            var from = moment(req.params.from, 'YYYYMMDD').format();
+            var to = moment(req.params.to, 'YYYYMMDD').format();
+            console.log(from, to);
+            getOperations(from, to).then(r=> {
                 res.json(r);
-            }).catch(e=> res.json(e));
+            }).catch(e=> {
+              res.status(500).send('Something broke! -> ' + e);
+            });
         });
     }
 }
